4909c03f21e2c445b329e91906c1c6cc7bcc8fc9
Sean C Davis <scdavis41@gmail.com>
2023-03-03 10:18:49 -0500

    Make page loading more consistent among the examples

--- 02-page-templates/utils/framework.js
+++ 02-page-templates/utils/framework.js
@@ -3,46 +3,68 @@
 import ejs from 'ejs';
 import fs from 'fs';
 import path from 'path';
+import glob from 'fast-glob';
 
 const IS_DEV = process.argv.includes('dev');
 const SRC_DIR = path.join(process.cwd(), 'src');
 const DIST_DIR = path.join(process.cwd(), 'dist');
-const CONTENT_FILE = path.join(SRC_DIR, 'content.json');
-const LAYOUT_FILE = path.join(SRC_DIR, 'layout.ejs');
+const PAGES_DIR = path.join(SRC_DIR, 'content/pages');
+const LAYOUTS_DIR = path.join(SRC_DIR, 'layouts');
+
+// Global refs — they get updated on each build
+let layouts = {};
 
 // Prepare dist directory
 if (fs.existsSync(DIST_DIR)) fs.rmSync(DIST_DIR, { recursive: true, force: true });
 fs.mkdirSync(DIST_DIR);
 
-function buildPage(page, layout) {
-  const urlPath = page.urlPath + (page.urlPath.endsWith('/') ? 'index' : '') + '.html';
-  const filePath = path.join(DIST_DIR, urlPath);
+function buildPage(relSrcFilePath) {
+  // Parse page and retrieve layout
+  const absSrcFilePath = path.join(PAGES_DIR, relSrcFilePath);
+  const rawPageContent = fs.readFileSync(absSrcFilePath, 'utf-8').toString();
+  const page = JSON.parse(rawPageContent);
+  const layout = layouts[page.layout];
+  // Escape if layout doesn't exist
+  if (!layout) console.error(`Layout "${page.layout}" not found for page "${relSrcFilePath}"`);
+  // Get and set urlPath on page from file path
+  const urlPath = relSrcFilePath
+    .replace(/\.json$/, '/index.html')
+    .replace(/\/index\/index\.html$/, '/index.html')
+    .replace(/^index\/index\.html$/, 'index.html');
+  // Determine destination file path
+  const distFilePath = path.join(DIST_DIR, urlPath);
   // Create directory if it doesn't exist
-  const dirPath = path.dirname(filePath);
+  const dirPath = path.dirname(distFilePath);
   if (!fs.existsSync(dirPath)) fs.mkdirSync(dirPath, { recursive: true });
   // Run page through EJS layout and write to file
-  const html = ejs.render(layout, { ...page });
-  fs.writeFileSync(filePath, html);
+  const html = ejs.render(layout, { page });
+  fs.writeFileSync(distFilePath, html);
+}
+
+function updateLayouts() {
+  const layoutFiles = glob.sync('**/*.ejs', { cwd: LAYOUTS_DIR });
+  layouts = Object.fromEntries(
+    layoutFiles.map((filePath) => {
+      const layoutName = path.basename(filePath, '.ejs');
+      const layout = fs.readFileSync(path.join(LAYOUTS_DIR, filePath), 'utf-8').toString();
+      return [layoutName, layout];
+    }),
+  );
 }
 
 function buildSite() {
-  // Read and parse content file
-  const content = JSON.parse(fs.readFileSync(CONTENT_FILE, 'utf-8').toString());
-  // Read layout file
-  const layout = fs.readFileSync(LAYOUT_FILE, 'utf-8').toString();
+  // Update global refs
+  updateLayouts();
+  // Get page files
+  const pageFiles = glob.sync('**/*.json', { cwd: PAGES_DIR });
   // Build each page
-  content.pages.forEach((page) => buildPage(page, layout));
+  pageFiles.forEach(buildPage);
   // Provide feedback
-  console.log(`Built ${content.pages.length} pages`);
+  console.log(`Built ${pageFiles.length} pages`);
 }
 
 // In dev mode, watch for changes to content.json and rebuild
-if (IS_DEV) {
-  fs.watch(SRC_DIR, { recursive: true }, () => {
-    console.log('Detected file change. Rebuilding ...');
-    buildSite();
-  });
-}
+if (IS_DEV) fs.watch(SRC_DIR, { recursive: true }, buildSite);
 
 // Do initial build
 buildSite();
---
+++ 03-stackbit-pages/stackbit.config.ts
@@ -0,0 +1,22 @@
+import { defineStackbitConfig } from '@stackbit/types';
+
+export const config = defineStackbitConfig({
+  stackbitVersion: '~0.6.0',
+  ssgName: 'nextjs',
+  cmsName: 'git',
+  nodeVersion: '16',
+  models: {
+    page: {
+      type: 'page',
+      urlPath: '/{slug}',
+      filePath: '{slug}.json',
+      hideContent: true,
+      fields: [{ name: 'title', type: 'string', required: true }],
+    },
+  },
+  pagesDir: 'src/content/pages',
+  pageLayoutKey: 'type',
+  objectTypeKey: 'type',
+});
+
+export default config;
